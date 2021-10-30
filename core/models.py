ddadc805af4f5ee3c9a8b931280af7eb302f601f
Siddharth <siddharthbora170@gmail.com>
2021-10-30 21:06:11 +0530

    pharma added

--- core/models.py
+++ core/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.db.models.base import Model
 from django.db.models.deletion import CASCADE
 from django.db.models.expressions import Case
+from django.db.models.fields import AutoField
 from django.db.models.fields.related import OneToOneField
 import uuid
 
@@ -11,48 +12,52 @@ import uuid
 # Create your models here.
 
 class Doctor(models.Model):
-    user= models.OneToOneField(User, on_delete=CASCADE)
-    doctor_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    doc_details = models.TextField(max_length=500, null= True, blank= True)
-    edu_details = models.TextField(max_length=500, null= True, blank= True)
+    user = models.OneToOneField(User, on_delete=CASCADE)
+    doctor_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    doc_details = models.TextField(max_length=500, null=True, blank=True)
+    edu_details = models.TextField(max_length=500, null=True, blank=True)
     Address = models.TextField(max_length=100, blank=True, null=True)
     contact = models.CharField(max_length=10, blank=True, null=True)
     clinic_name = models.CharField(max_length=100, blank=True, null=True)
 
 
-
-
 class Patient(models.Model):
     user = models.OneToOneField(User, on_delete=CASCADE)
-    patient_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    patient_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
     dob = models.DateField(blank=True, null=True)
     height = models.IntegerField(blank=True, null=True)
-    weight = models.IntegerField(blank= True, null= True)
-    bloodgroup = models.CharField(max_length=5,blank= True, null= True)
+    weight = models.IntegerField(blank=True, null=True)
+    bloodgroup = models.CharField(max_length=5, blank=True, null=True)
     contact = models.CharField(max_length=10, blank=True, null=True)
-    pre_med_hist = models.TextField(max_length=100, blank= True, null= True)
+    pre_med_hist = models.TextField(max_length=100, blank=True, null=True)
 
-  
     def getuuid(self):
         return self.patient_id
+
+
+class Pharmacy(models.Model):
+    Pharma_ID = models.CharField(max_length=50, blank=True, null=True)
+    Address = models.TextField(max_length=100, blank=True, null=True)
+    contact = models.CharField(max_length=10, blank=True, null=True)
+    Phara_certi_id = models.CharField(max_length=50, blank=True, null=True)
 
 
 class Prescription(models.Model):
     doctor_id = models.ForeignKey(Doctor, on_delete=CASCADE)
     patient_id = models.ForeignKey(Patient, on_delete=CASCADE)
+    Pharma_ID = models.ForeignKey(Pharmacy, on_delete=CASCADE)
     fees = models.IntegerField(blank=True, null=True)
-    date = models.DateField(blank=True, null= True)
-    status = models.BooleanField(default=False);
-
-
+    date = models.DateField(blank=True, null=True)
+    status = models.BooleanField(default=False)
 
 
 class Medecine(models.Model):
     med_name = models.CharField(max_length=50, blank=True, null=True)
-    qty = models.IntegerField(blank= True, null= True)
+    qty = models.IntegerField(blank=True, null=True)
     prescription = models.ForeignKey(Prescription, on_delete=CASCADE)
-    dose = models.CharField(blank=True, null=True, default="",max_length=100)
+    dose = models.CharField(blank=True, null=True, default="", max_length=100)
 
     def __str__(self):
         return self.med_name
-    
--- core/views.py
+++ core/views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib.auth import authenticate, login, logout
 from django.urls.resolvers import LocalePrefixPattern
-from .models import Doctor , Patient, Prescription, Medecine
+from .models import Doctor, Patient, Pharmacy, Prescription, Medecine
 
 # Create your views here.
+
+
 def signin(request):
     login_page_path = 'login.html'
 
@@ -21,22 +23,22 @@ def signin(request):
         data = request.POST
         username = data['username']
         password = data['password']
-        
+
         user = authenticate(request, username=username, password=password)
         if user:
             if (user.is_staff):
 
                 auth.login(request, user)
-            
+
                 return redirect("ddash")
             else:
-                
-                patient = Patient.objects.get(user = user)
+
+                patient = Patient.objects.get(user=user)
                 auth.login(request, user)
                 return redirect("pdash")
         return render(request, login_page_path, {'msg': 'Invalid Credentials!'})
     return render(request, login_page_path)
-   
+
 
 def signupd(request):
     register_doctor = 'doctor_signup.html'
@@ -58,29 +60,30 @@ def signupd(request):
 
         if password != password2:
             return render(request, register_doctor, {'msg': ["Passwords Don't match"]})
-      
+
         if len(password) == 0:
             return render(request, register_doctor, {'msg': ["Please enter password"]})
 
         try:
-            user1 = User.objects.create(username = username, first_name = fname, last_name = lname, email = email, password = password)
+            user1 = User.objects.create(
+                username=username, first_name=fname, last_name=lname, email=email, password=password)
             Doctor.objects.create(
-                user = user1,
-                edu_details = edeatil,
-                doc_details = ddetail,
-                Address = address,
-                contact = phone,
-                clinic_name = clinic_name,
-                )
+                user=user1,
+                edu_details=edeatil,
+                doc_details=ddetail,
+                Address=address,
+                contact=phone,
+                clinic_name=clinic_name,
+            )
             user1.save()
             auth.login(request, user1)
             return redirect('doctor_dashboard')
         except Exception as e:
             return render(request, register_doctor, {'msg': [f'User already exists..!!{e}']})
-            
+
     return render(request, register_doctor)
-  
-    
+
+
 def signupp(request):
     register_patient = 'patient_signup.html'
     if request.method == 'POST':
@@ -103,97 +106,114 @@ def signupp(request):
 
         if password != password2:
             return render(request, register_patient, {'msg': ["Passwords Don't match"]})
-      
+
         if len(password) == 0:
             return render(request, register_patient, {'msg': ["Please enter password"]})
 
         try:
-            user1 = User.objects.create(username = username, first_name = fname, last_name = lname, email = email, password = password)
+            user1 = User.objects.create(
+                username=username, first_name=fname, last_name=lname, email=email, password=password)
             Patient.objects.create(
-                user = user1,
-                dob = dob,
-                height = height,
-                weight = weight,
-                bloodgroup = bloodgroup,
-                contact = contact,
-                pre_med_hist = pre_hist
-                )
+                user=user1,
+                dob=dob,
+                height=height,
+                weight=weight,
+                bloodgroup=bloodgroup,
+                contact=contact,
+                pre_med_hist=pre_hist
+            )
             user1.save()
             auth.login(request, user1)
             return redirect('patient_dashboard')
         except Exception as e:
             return render(request, register_patient, {'msg': [f'User already exists..!!{e}']})
-  
+
     return render(request, register_patient)
-  
+
 
 def doctord(request):
     ddash = 'doctor_dashboard.html'
     if request.user.is_authenticated and request.user.is_staff:
-        d_obj = Doctor.objects.get(user = request.user)
-        pre_obj = Prescription.objects.filter(doctor_id = d_obj)
+        d_obj = Doctor.objects.get(user=request.user)
+        pre_obj = Prescription.objects.filter(doctor_id=d_obj)
         t_data = {}
         for item in pre_obj:
             med = Medecine.objects.filter(prescription=pre_obj)
             t_data[item.pk] = med
         data = {
-            'd_obj':d_obj,
-            'pre_obj':pre_obj,
-         
-            't_data':t_data
+            'd_obj': d_obj,
+            'pre_obj': pre_obj,
+
+            't_data': t_data
         }
         return render(request, ddash, data)
 
     return redirect('login')
-   
+
 
 def patientd(request):
-    
+
     pdash = 'patient_dashboard.html'
     print(request.user.is_authenticated)
     if request.user.is_authenticated:
-      
-        p_obj = Patient.objects.get(user = request.user)
-      
-        pre_obj = Prescription.objects.filter(patient_id = p_obj)
-        
-        t_data= {}
+
+        p_obj = Patient.objects.get(user=request.user)
+        pre_obj = Prescription.objects.filter(patient_id=p_obj)
+        t_data = {}
         for item in pre_obj:
-            meditem = Medecine.objects.filter(prescription = item)      
+            meditem = Medecine.objects.filter(prescription=item)
             t_data[item] = meditem
-               
-        
+
         data = {
-            'p_obj':p_obj,
-            'pre_obj':pre_obj,
-            't_data':t_data
-         
+            'p_obj': p_obj,
+            'pre_obj': pre_obj,
+            't_data': t_data
         }
         for item in t_data:
-            print (t_data[item])
-
+            print(t_data[item])
+        # ===================== take a variable from front end to get priscriptiomn================
+        data = request.POST
+        alpha = data['Pharmacy']
+        p_obj.Pharma_ID = alpha
+        p_obj.save()
         return render(request, pdash, data)
     print("Not authen")
     return redirect('login')
-   
+
+
+def Pharma(request):
+
+    pdash = 'pharmacy_dashboard.html'
+    print(request.user.is_authenticated)
+    if request.user.is_authenticated:
+        pharma_obj = Pharmacy.objects.get(user=request.user)
+        pres = Prescription.objects.filter(Pharma_ID=pharma_obj)
+        #data={'pres': pres}
+        # store data
+        return render(request, pdash, pres)
+    print("Not authen")
+    return redirect('login')
+
+
 def signout(request):
     try:
         logout(request)
         return HttpResponse("logouted successfully")
     except:
-       return HttpResponse("Need to Login First")
+        return HttpResponse("Need to Login First")
 
 
 def new(request):
     return render(request, 'new_presc.html')
+
 
 def getp(request):
     return JsonResponse("hello", 200)
     print("reques")
     pid = request.POST['pid']
     p_obj = Patient.objects.get(patient_id=pid)
-    return render(request, 'pdetail.html', {'pobj':p_obj })
+    return render(request, 'pdetail.html', {'pobj': p_obj})
     # if p_obj:
-        
+
     # print("invlaid")
     # return HttpResponse("invalid Patient ID")
